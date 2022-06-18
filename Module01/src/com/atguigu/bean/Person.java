6638ba69c812d5d5d644dc0208be01fe38194960
lenovo <fffyd931115@outlook.com>
2022-06-18 10:12:02 +0800

    上传文件

--- Module01/src/com/atguigu/bean/Person.java
+++ Module01/src/com/atguigu/bean/Person.java
@@ -5,6 +5,11 @@ package com.atguigu.bean;
  * @date 2022/6/15 - 10:10
  */
 public class Person {
+    /**
+    * 姓名
+    */
+    private String NAME;
+
 
     public void eat (){
         System.out.println("eat rice");
---
+++ Module01/src/com/atguigu/bean/Test.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author fyd
+ * @date 2022/6/18 - 10:07
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
